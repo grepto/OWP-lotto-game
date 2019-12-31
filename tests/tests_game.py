30a0ae7d970e64cb8a01d5f0ddc1bf09781ccf08
Alexandr Popov <grepto@gmail.com>
2019-12-31 21:44:24 +0700

    mock is_winner attribute for human_player

--- tests/tests_game.py
+++ tests/tests_game.py
@@ -11,6 +11,11 @@ def game(robot_player, human_player):
     return game
 
 
+@pytest.fixture
+def human_is_winner(mocker):
+    return mocker.patch('src.game.HumanPlayer.is_winner')
+
+
 def test_game_init(game):
     assert game.cards_per_player == 2
     assert game.robots_number == 1
@@ -39,8 +44,7 @@ def test_get_winner_through_looser(game, human_player, robot_player):
     assert game.get_winner() == robot_player
 
 
-def test_get_winner_through_winner(game, human_player, barrels):
-    human_player.cards = [Card(barrels)]
-    human_player.cards[0]._clear_numbers()
+def test_get_winner_through_winner(game, human_player, human_is_winner):
+    human_is_winner.return_value = True
 
     assert game.get_winner() == human_player
