ff439b745f9294e6ef8261cecec63eb62a74d785
Alexandr Popov <grepto@gmail.com>
2019-12-31 21:13:27 +0700

    move pouch fixture to this tests file

--- tests/tests_pouch.py
+++ tests/tests_pouch.py
@@ -1,5 +1,12 @@
 import pytest
 
+from src.game import Pouch
+
+
+@pytest.fixture
+def pouch(barrels, random_shuffle):
+    return Pouch(barrels)
+
 
 def test_pouch_getting_barrels(pouch, barrels):
     for barrel in barrels:
